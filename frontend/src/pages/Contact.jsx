b990b18dc59155d0bffbba8f4dd2f1d7e31d3e73
shubhamaaaa <130844125+shubhamaaaa@users.noreply.github.com>
2025-04-05 10:59:34 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
     setResult("Sending...");
 
     try {
-      const response = await fetch("http://localhost:3000/api/contact/add", {
+      const response = await fetch("https://mitli.in/api/contact/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
