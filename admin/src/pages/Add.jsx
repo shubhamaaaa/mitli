591f5312b8982c38b8c951711ef6df63dd698ae4
shubhamaaaa <130844125+shubhamaaaa@users.noreply.github.com>
2025-04-08 17:08:41 +0530

    Update Add.jsx

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -41,7 +41,7 @@ const Add = ({ token }) => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/product/add",
+        "https://mitli.in/api/product/add",
         formDataToSend,
         {
           headers: { token, "Content-Type": "multipart/form-data" },
